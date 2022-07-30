b080d1c78b6d82f0434ad6b33cb1e7b2b8e960f2
Luke Sy <sylukewicent@gmail.com>
2022-07-31 09:47:00 +1000

    Added Soln to Ex 3.12

---
+++ mongo.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const url = `mongodb+srv://fullstack:${password}@cluster0.2mwywxr.mongodb.net/?retryWrites=true&w=majority`
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length == 3) {
+  mongoose
+    .connect(url)
+    .then((result) => {
+      console.log('connected')
+
+      return Person.find({})
+    })
+    .then((result) => {
+        console.log('phonebook:')
+        result.forEach(p => {
+          console.log(`${p.name} ${p.number}`)
+        })
+      mongoose.connection.close()
+    })
+    .catch((err) => console.log(err))
+} else {
+  mongoose
+    .connect(url)
+    .then((result) => {
+      console.log('connected')
+
+      const p = new Person({
+        name: process.argv[3],
+        number: process.argv[4]
+      })
+
+      return p.save()
+    })
+    .then((person) => {
+      console.log(`added ${person.name} number ${person.number} to phonebook`)
+
+      return mongoose.connection.close()
+    })
+    .catch((err) => console.log(err))
+}
