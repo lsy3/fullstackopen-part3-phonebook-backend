338bdf60030c76dd2c6db21d1f1d278d990a66e3
Luke Sy <sylukewicent@gmail.com>
2022-07-31 17:35:34 +1000

    Added solution to Ex 3.15 - 3.18

--- index.js
+++ index.js
@@ -30,6 +30,10 @@ const MAX_ID = 10000
 //   }
 // ]
 
+app.use(express.static('build'))
+app.use(express.json())
+app.use(cors())
+
 morgan.token('postdata', function getPostData(req) {
   let ret = ' '
   if (req['_body']) {
@@ -38,18 +42,20 @@ morgan.token('postdata', function getPostData(req) {
   return ret
 })
 
-app.use(express.static('build'))
-app.use(express.json())
-app.use(cors())
 // app.use(morgan('tiny'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postdata'))
+
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
 app.get('/info', (req, res) => {
-  res.send(`Phonebook has info for ${persons.length} people<br\>${new Date()}`)
+  Person.find({})
+    .then(results => {
+      res.send(`Phonebook has info for ${results.length} people<br\>${new Date()}`)
+    })
+
 })
 
 // const generateId = () => {
@@ -97,21 +103,61 @@ app.get('/api/persons', (req, res) => {
   })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(p => p.id !== id)
-
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then(p => {
-      response.json(p)
+      if (p) {
+        response.json(p)
+      } else {
+        response.status(404).end()
+      }
     })
+    .catch(error => next(error))
 
   // response.status(404).end()
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
